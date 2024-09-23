885514cb3f517ab164ce3f10f43746077f1342c0
Bocanasa, LLC <info@bocanasa.com>
2024-09-23 16:32:14 -0500

    Initial Commit

---
+++ CVS/CVSApp.swift
@@ -0,0 +1,17 @@
+//
+//  CVSApp.swift
+//  CVS
+//
+//  Created by Done Santana on 9/23/24.
+//
+
+import SwiftUI
+
+@main
+struct CVSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CVSTests/CVSTests.swift
@@ -0,0 +1,17 @@
+//
+//  CVSTests.swift
+//  CVSTests
+//
+//  Created by Done Santana on 9/23/24.
+//
+
+import Testing
+@testable import CVS
+
+struct CVSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
